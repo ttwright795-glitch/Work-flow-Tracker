74b2394a345737aee378fb32d5b7d34b582fdec6
ttwright795-glitch <ttwright795@gmail.com>
2025-09-24 22:52:37 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,5 @@
-self.addEventListener('fetch',e=>{});
+const CACHE='workflow-tracker-v2';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim())});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
